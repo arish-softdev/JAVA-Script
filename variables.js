76c71b7ccece1bb4507bf606ba58f1a350afca8c
arish-softdev <arish.softdev@gmail.com>
2025-10-31 15:11:51 +0530

    variables

--- variables.js
+++ variables.js
@@ -1,15 +1,46 @@
-var a = 5;
-var b = 10;
+// var a = 5;
+// var b = 10;
 
-function add() {
-  console.log(a + b);
-}
+// function add() {
+//   console.log(a + b);
+// }
 
-add();
+// add();
 
-function sub() {
-  console.log(a - b);
-}
+// function sub() {
+//   console.log(a - b);
+// }
 
-sub();
+// sub();
+
+// const a = 1;
+// console.log(a);
+
+// let a = 15;
+// a=10;
+// console.log(a);
+
+// console.log(c);
+
+// {
+//   let c = 19;
+// }
+
+// let a=5;
+
+// function run() {
+//   let a=10;
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// run();
+
+// var a;
+// var a=10;
+// var a=15;
+// var a=89;
+
+// console.log(a);
 
