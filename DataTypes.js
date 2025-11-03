f1c67bdf8011622b8cbe291be0c8c79ec2bb55fd
arish-softdev <arish.softdev@gmail.com>
2025-11-03 22:41:59 +0530

    DATA TYPES

--- DataTypes.js
+++ DataTypes.js
@@ -1,4 +1,22 @@
-let a = 'Aqua';
-console.log(a);
+// let a = 'Aqua';
+// console.log(a);
 
+// let fname = aqua;
+// console.log("fname");
 
+// Number data type
+// let age = 25;
+// console.log(age);
+
+// String data type
+// let fname = "John Doe";
+// console.log(fname);
+
+// Boolean data type
+let isStudent = true;
+
+// Null data type
+let nullValue = null;
+
+// Undefined data type
+let undefinedValue;
