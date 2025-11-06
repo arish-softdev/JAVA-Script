2ec4f1a883cdc4549f5433a71748a7f0f6b073aa
arish-softdev <arish.softdev@gmail.com>
2025-11-06 23:32:47 +0530

    Logical Operators:

--- Operators.js
+++ Operators.js
@@ -12,12 +12,14 @@
 
 // Comparison Operators:
 
-const a = 20;
-const b = 5;
+// const a = 20;
+// const b = 5;
 
-console.log(a > b);
-console.log(a < b);
-console.log(a >= b);
-console.log(a <= b);
-console.log(a === b);
-console.log(a !== b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a === b);
+// console.log(a !== b);
+
+Logical Operators:
