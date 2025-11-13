39803ecb6bb39977705879660151b36b4a58b338
arish-softdev <arish.softdev@gmail.com>
2025-11-13 22:26:14 +0530

    If Statement

--- conditional_statements/if_statement.js
+++ conditional_statements/if_statement.js
@@ -1,6 +1,7 @@
- let age = 20;
-      if (age >= 28) {
-        console.log("You are eligible to vote.");
-      } else {
-        console.log("You are not eligible to vote yet.");
-      }
+let age = 20;
+
+if (age >= 28) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote yet.");
+}
