ffb1b40ee771e338d3a62e41be5623386a4eb3cc
arish-softdev <arish.softdev@gmail.com>
2025-11-14 23:48:22 +0530

    else

--- conditional_statements/else_statement.js
+++ conditional_statements/else_statement.js
@@ -1,12 +1,22 @@
-let day = "Friday";
+// let day = "Friday";
   
-  if (day === "Saturday" || day === "Sunday") {
-        console.log("It's the weekend!");
+//   if (day === "Saturday" || day === "Sunday") {
+//         console.log("It's the weekend!");
 
-} else if (day === "Friday") {
-        console.log("It's Friday! Almost the weekend.");
-} else {
-        console.log("It's a weekday.");
-}
+// } else if (day === "Friday") {
+//         console.log("It's Friday! Almost the weekend.");
+// } else {
+//         console.log("It's a weekday.");
+// }
 
 // 
+
+let fname = "Aqua";
+let fname2 = "Aqua";
+
+if (fname==fname2) {
+    console.log("same");
+    
+} else {
+    console.log("not same")
+}
