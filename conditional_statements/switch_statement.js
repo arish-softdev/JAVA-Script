47bd4b25ec6e752f356450ef77896cf9ba388025
arish-softdev <arish.softdev@gmail.com>
2025-11-15 00:05:37 +0530

    node switch_statement.js

---
+++ conditional_statements/switch_statement.js
@@ -0,0 +1,23 @@
+// Switch Case
+
+ let dayOfWeek = "sunday";
+  
+      switch (dayOfWeek) {
+        case "Monday":
+          console.log("It's Monday.");
+          break;
+        case "Tuesday":
+          console.log("It's Tuesday.");
+          break;
+        case "Wednesday":
+          console.log("It's Wednesday.");
+          break;
+        case "Thursday":
+          console.log("It's Thursday.");
+          break;
+        case "Friday":
+          console.log("It's Friday.");
+          break;
+        default:
+          console.log("It's a weekend day.");
+      }
