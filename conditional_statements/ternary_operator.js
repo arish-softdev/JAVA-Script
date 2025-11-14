461900adcdf208484999edb59d8c78f878bc0ea1
arish-softdev <arish.softdev@gmail.com>
2025-11-15 00:15:47 +0530

    node ternary_operator.js

--- conditional_statements/ternary_operator.js
+++ conditional_statements/ternary_operator.js
@@ -1,4 +1,10 @@
-let isAuthenticated = false;
+// let isAuthenticated = false;
   
-      let message = isAuthenticated ? "Welcome back!" : "Please log in.";
-      console.log(message);
+//       let message = isAuthenticated ? "Welcome back!" : "Please log in.";
+//       console.log(message);
+
+
+
+let avg = 80;
+let grade=avg>90? "A+" : avg>=80? "B+" : "C"
+console.log("grade:",grade);
