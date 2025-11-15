3a4d19754f0d9b383c3ec39008af4641d8a3617e
arish-softdev <arish.softdev@gmail.com>
2025-11-15 22:46:26 +0530

    ternary operator

--- conditional_statement/ternary_operator.js
+++ conditional_statement/ternary_operator.js
@@ -5,6 +5,11 @@
 
 
 
-let avg = 80;
-let grade=avg>90? "A+" : avg>=80? "B+" : "C"
-console.log("grade:",grade);
+// let avg = 80;
+// let grade=avg>90? "A+" : avg>=80? "B+" : "C"
+// console.log("grade:",grade);
+
+let age = 20;
+let vote=age>=18?"eligible to vote" :"not eligible to vote"
+
+console.log(vote);
