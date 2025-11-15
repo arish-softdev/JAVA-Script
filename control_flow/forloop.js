de002acca7bd79f704cb3a2d98ff27d4ceee76c3
arish-softdev <arish.softdev@gmail.com>
2025-11-15 23:16:20 +0530

    forloop

--- control_flow/forloop.js
+++ control_flow/forloop.js
@@ -5,9 +5,35 @@
 
 // ex2
 
-for (let i = -10; i <=-1; i++) {
-    console.log(i);
+// for (let i = -10; i <=-1; i++) {
+//     console.log(i);
     
-}
+// }
+
+// ex3
+
+// for (let i = 0; i <=5; i++) {
+//     console.log(i);
+    
+// }
+
+//  ex4 post increment 
+// let x=10;
+// let y=x++;
+
+// console.log("the value of x:",x ,"the value of y:",y);
+
+// ex5 preincrement
+
+// let a=10;
+// let b=++a;
+
+// console.log("the value of x:",a ,"the value of y:",b);
 
 // 
+
+
+for (let i = 0; i <=5; i++) {
+    console.log("aqua");
+    
+}
