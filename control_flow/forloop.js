01a0001ce977d95b21ab9c4d91b55e2d770070a7
arish-softdev <arish.softdev@gmail.com>
2025-11-15 22:15:47 +0530

    forloop

--- control_flow/forloop.js
+++ control_flow/forloop.js
@@ -1,4 +1,13 @@
-for (let i = 10; i >=5; i--) {
+// for (let i = 10; i >=5; i--) {
+//     console.log(i);
+    
+// }
+
+// ex2
+
+for (let i = -10; i <=-1; i++) {
     console.log(i);
     
 }
+
+// 
