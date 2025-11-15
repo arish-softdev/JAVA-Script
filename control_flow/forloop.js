426218b59a31b93bcb2ca0781c03bff6a242e464
arish-softdev <arish.softdev@gmail.com>
2025-11-15 23:28:52 +0530

    forloop

--- control_flow/forloop.js
+++ control_flow/forloop.js
@@ -39,9 +39,17 @@
 
 // 
 
-for (let i = 2; i <=20; i++) {
-    if (i%2==0) {
-        console.log(i);
+// for (let i = 2; i <=20; i++) {
+//     if (i%2==0) {
+//         console.log(i);
         
-    }
+//     }
+// }
+
+// 
+
+let name = "Aquamarine";
+for (let i = 3; i < name.length; i++) {
+    console.log(name[i]);
+    
 }
