ab2a27a46ebc0b57f03ecd6a726744f9b9ef8a0c
arish-softdev <arish.softdev@gmail.com>
2025-11-16 19:17:49 +0530

    do while

--- control_flow/DoWhileloop.js
+++ control_flow/DoWhileloop.js
@@ -1,8 +1,17 @@
 // Ex1 Print 1 to 10
 
-let i = 1;
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+// Ex2 Print even numbers from 2 to 10
+
+let i = 2;
 
 do {
     console.log(i);
-    i++;
+    i +=2;
 } while (i <= 10);
