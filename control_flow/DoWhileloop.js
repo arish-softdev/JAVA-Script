07141b92a3ce63de999976fcc33b25fdc3009af3
arish-softdev <arish.softdev@gmail.com>
2025-11-17 16:40:46 +0530

    Do while

--- control_flow/DoWhileloop.js
+++ control_flow/DoWhileloop.js
@@ -9,9 +9,18 @@
 
 // Ex2 Print even numbers from 2 to 10
 
-let i = 2;
+// let i = 2;
+
+// do {
+//     console.log(i);
+//     i +=2;
+// } while (i <= 10);
+
+// Ex3 Print multuples of 3 up to 30
+
+let q = 3;
 
 do {
-    console.log(i);
-    i +=2;
-} while (i <= 10);
+    console.log(q);
+    q += 3;
+} while (q <= 30);
