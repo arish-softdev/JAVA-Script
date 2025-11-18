a73ab49e9f01071ed292baef19e07c114eaa8f5c
arish-softdev <arish.softdev@gmail.com>
2025-11-18 12:04:41 +0530

    For...In Loop in javascript

--- control_flow/ForInloop.js
+++ control_flow/ForInloop.js
@@ -1,1 +1,12 @@
 // For...In Loop in javascript
+
+const person = {
+    name: "Cid",
+    age: 25,
+    profession: "Developer"
+};
+
+for (let key in person) {
+    console.log(key + ":" + person[key]);
+    
+}
