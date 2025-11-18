73fee1ee3420059ecda200f74c9ff810e5ee297d
arish-softdev <arish.softdev@gmail.com>
2025-11-18 12:11:16 +0530

    For...In Loop in javascript

--- control_flow/ForInloop.js
+++ control_flow/ForInloop.js
@@ -1,12 +1,26 @@
 // For...In Loop in javascript
+// EX1 Print all properties of an object
 
-const person = {
-    name: "Cid",
-    age: 25,
-    profession: "Developer"
+// const person = {
+//     name: "Cid",
+//     age: 25,
+//     profession: "Developer"
+// };
+
+// for (let key in person) {
+//     console.log(key + ":" + person[key]);
+    
+// }
+
+// EX2 Print all values of an object
+
+let car = {
+    brand: "BMW",
+    model: "M4",
+    year: 2022
 };
 
-for (let key in person) {
-    console.log(key + ":" + person[key]);
+for (let key in car) {
+    console.log(car[key]);
     
 }
