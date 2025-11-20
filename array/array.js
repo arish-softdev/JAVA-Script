b342a86bcba82715fcaab5317fa3dc69ebf36bea
arish-softdev <arish.softdev@gmail.com>
2025-11-20 23:23:48 +0530

    Array in javascript

--- array/array.js
+++ array/array.js
@@ -1,4 +1,10 @@
 // Array in javascript
 
-let animes = ["dragonball", "onepiece", "naruto"];
-console.log(animes);
+// let animes = ["dragonball", "onepiece", "naruto"];
+// console.log(animes);
+
+// Ex2 Array
+
+let oshinoko = ["Ai", "Aqua", "Ruby", "Kana", "Akane"];
+
+console.log(oshinoko[2]);
