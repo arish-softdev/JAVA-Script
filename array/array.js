985a1ccec8975d9c3ccd4f0033163e58806a309e
arish-softdev <arish.softdev@gmail.com>
2025-11-21 23:18:42 +0530

    Array in javascript

--- array/array.js
+++ array/array.js
@@ -1,23 +1,29 @@
 // Array in javascript
 
-let animes = ["dragonball", "onepiece", "naruto"];
-console.log(animes);
+// let animes = ["dragonball", "onepiece", "naruto"];
+// console.log(animes);
 
 // Ex2 Array
 
-let oshinoko = ["Ai", "Aqua", "Ruby", "Kana", "Akane"];
+// let oshinoko = ["Ai", "Aqua", "Ruby", "Kana", "Akane"];
 
-console.log(oshinoko[2]);
+// console.log(oshinoko[2]);
 
 // Ex3 Array
 
-let info = [name='robin', age=19, city='vadalur'];
+// let info = [name='robin', age=19, city='vadalur'];
 
-console.log(info);
+// console.log(info);
 
 // Ex4 change array value
 
-let fruits = ["apple", "orange", "banana"];
-fruits[2] = "grapes";
+// let fruits = ["apple", "orange", "banana"];
+// fruits[2] = "grapes";
 
-console.log(fruits);
+// console.log(fruits);
+
+// Ex5 Find length of array
+
+let colors = ["red", "green", "black", "blue"];
+
+console.log(colors.length);
