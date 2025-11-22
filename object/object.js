f24d28f79bdcac812d0057d87297f17ecf4164bd
arish-softdev <arish.softdev@gmail.com>
2025-11-22 23:32:36 +0530

    Object in Javascript

--- object/object.js
+++ object/object.js
@@ -1,11 +1,22 @@
 // Object in Javascript
 // Ex1
 
-const person = {
-    name: "Arish",
-    age: 19,
-    profession: "Developer"
-};
+// const person = {
+//     name: "Arish",
+//     age: 19,
+//     profession: "Developer"
+// };
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.profession);
+
+// Ex2 
+
+const person = new Object();
+person.name = "Arish";
+person.age = 19;
+person.profession = "Developer";
 
 console.log(person.name);
 console.log(person.age);
