1e4b9a5fcc6d00b630ccfa1a25c51fb4846a89f2
arish-softdev <arish.softdev@gmail.com>
2025-11-22 23:37:36 +0530

    Object in Javascript

--- object/object.js
+++ object/object.js
@@ -13,11 +13,25 @@
 
 // Ex2 
 
-const person = new Object();
-person.name = "Arish";
-person.age = 19;
-person.profession = "Developer";
+// const person = new Object();
+// person.name = "Arish";
+// person.age = 19;
+// person.profession = "Developer";
 
-console.log(person.name);
-console.log(person.age);
-console.log(person.profession);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.profession);
+
+// Ex3
+
+const person = {
+    name: "Robin",
+    age: 22,
+    profession: "Hunter"
+};
+
+const propertyName = "age";
+console.log(person[propertyName]);
+
+const prifessionKey = "profession";
+console.log(person.professionKey);
