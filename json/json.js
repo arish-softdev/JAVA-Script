30507b0ca622a6acccf275790a700ccc3863c79e
arish-softdev <arish.softdev@gmail.com>
2025-11-22 23:55:38 +0530

    JSON in Javascript

--- json/json.js
+++ json/json.js
@@ -1,10 +1,21 @@
 // JSON in Javascript
+// Ex1
 
-let person = {
-    name: "Arish",
-    age: 19,
-    city: "Vadalur"
-};
+// let person = {
+//     name: "Arish",
+//     age: 19,
+//     city: "Vadalur"
+// };
 
-let jsonString = JSON.stringify(person);
-console.log(jsonString);
+// let jsonString = JSON.stringify(person);
+// console.log(jsonString);
+
+// Ex2
+
+const jsonString = '{"name":"Arish", "age":30, "city":"Vadalur"}';
+
+const person = JSON.parse(jsonString);
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
