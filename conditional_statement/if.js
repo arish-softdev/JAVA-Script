c7783d4dfa9476f05b0f8be03ec405f34b99de24
arish-softdev <arish.softdev@gmail.com>
2025-11-23 19:09:24 +0530

    else if in javascript

--- conditional_statement/if.js
+++ conditional_statement/if.js
@@ -1,10 +1,23 @@
 // Check even or odd
 
-let num = 7;
+// let num = 7;
 
-if (num % 2 === 0) {
-    console.log("Even");
+// if (num % 2 === 0) {
+//     console.log("Even");
     
+// } else {
+//     console.log("Odd");
+// }
+
+// Check positive, negative, or zero
+
+let num = 10;
+
+if (num > 0) {
+    console.log("Positive");
+
+} else if (num < 0) {
+    console.log("Negative")
 } else {
-    console.log("Odd");
+    console.log("Zero")
 }
