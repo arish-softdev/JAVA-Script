47efca52a159577ba91ce3987396a13735575388
arish-softdev <arish.softdev@gmail.com>
2025-11-23 19:18:29 +0530

    if in javascript

--- conditional_statement/if.js
+++ conditional_statement/if.js
@@ -11,13 +11,24 @@
 
 // Check positive, negative, or zero
 
-let num = 10;
+// let num = 10;
 
-if (num > 0) {
-    console.log("Positive");
+// if (num > 0) {
+//     console.log("Positive");
 
-} else if (num < 0) {
-    console.log("Negative")
+// } else if (num < 0) {
+//     console.log("Negative")
+// } else {
+//     console.log("Zero")
+// }
+
+// largest of two numbers
+
+let a = 10; b = 20;
+
+if (a > b) {
+    console.log("a is larger");
+    
 } else {
-    console.log("Zero")
+    console.log("b is larger");
 }
