23fdf1339ce55c1bc3c69e06ca18b5528cb45b7c
arish-softdev <arish.softdev@gmail.com>
2025-11-25 23:44:02 +0530

    Array method in javasript

--- array/arraymethod.js
+++ array/arraymethod.js
@@ -1,6 +1,11 @@
-// Array method in javascrit 
+// Array method in javascript 
 
-let arr = [1, 2, 3];
-arr.push(4);
-console.log(arr); 
+// let arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr); 
+
+// Ex2 
+
+let arr = [5, 10, 15];
+arr.shift();  
 
