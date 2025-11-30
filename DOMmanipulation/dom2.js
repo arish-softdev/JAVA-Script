c27aa700eecbb5a1b43c1f671981c27e069b0d88
arish-softdev <arish.softdev@gmail.com>
2025-11-30 23:23:56 +0530

    DOM Manipulation in JavaScript

--- DOMmanipulation/dom2.js
+++ DOMmanipulation/dom2.js
@@ -1,3 +1,8 @@
 // Ex1 Change the of an element
 
 document.getElementById("msg").textContent = "Welcome!";
+
+// CSS style using JS
+
+let myElement = document.getElementById("box")
+myElement.style.backgroundColor = "red";
