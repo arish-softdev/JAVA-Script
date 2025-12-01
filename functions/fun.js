9e7f64e0f5525952b47c282a1589e99cff1d139c
arish-softdev <arish.softdev@gmail.com>
2025-12-01 16:09:19 +0530

    Functions in JavaScript

--- functions/fun.js
+++ functions/fun.js
@@ -1,8 +1,19 @@
 // Functions in JavaScript
 // Ex1 Add two numbers
 
-function add(a, b) {
-    return a + b;
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(5, 3));
+
+// Ex2 Check if a number is even or odd
+
+function checkEvenOdd (num) {
+    if (num % 2 === 0) {
+        return "Even";
+    }
+    return "Odd";
 }
 
-console.log(add(5, 3));
+console.log(checkEvenOdd(7));
