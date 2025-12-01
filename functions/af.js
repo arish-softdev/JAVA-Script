2eb6cbeae7b42859ff51518dc3b00756b4cbece6
arish-softdev <arish.softdev@gmail.com>
2025-12-01 16:40:45 +0530

    Arrow Function in JavaScript

--- functions/af.js
+++ functions/af.js
@@ -1,6 +1,12 @@
 // Arrow Function in JavaScript
 // Ex1
 
-const add = (a, b) => a + b;
+// const add = (a, b) => a + b;
 
-console.log(add(1, 9));
+// console.log(add(1, 9));
+
+// Ex2 
+
+let greet = name => "Hi " + name;
+
+console.log(greet("Soldiers"));
