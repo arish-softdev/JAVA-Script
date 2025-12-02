f5c1df73abdf4de8477f9c3c0d2bfab4200d804f
arish-softdev <arish.softdev@gmail.com>
2025-12-02 22:58:02 +0530

    Closure in JavaScriptl

--- scope_closures/closure.js
+++ scope_closures/closure.js
@@ -1,28 +1,49 @@
 // Closure in JavaScript
 
+// function createCounter() {
+//     let count = 0;
+
+//     function increment() {
+//         count++;
+//         console.log(count);
+        
+//     }
+
+//     return increment;
+// }
+
+// const counter = createCounter();
+
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+// counter();
+
+// 
+
 function createCounter() {
     let count = 0;
 
-    function increment() {
-        count++;
+    function decrement() {
+        count--;
         console.log(count);
         
     }
 
-    return increment;
+    return decrement;
 }
 
-const counter = createCounter();
+let counter = createCounter();
 
-counter();
-counter();
-counter();
-counter();
-counter();
-counter();
-counter();
-counter();
-counter();
 counter();
 counter();
 counter();
