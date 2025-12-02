c64b983a5b79b918247297b05f8782530e9cf778
arish-softdev <arish.softdev@gmail.com>
2025-12-02 22:38:01 +0530

    Scope in JavaScript

--- scope_closures/scope.js
+++ scope_closures/scope.js
@@ -1,11 +1,23 @@
 // Scope in JavaScript
 // Global Scope
 
-const message = "Welcome to demon school..!";
+// const message = "Welcome to demon school..!";
+
+// function showMessage() {
+//     console.log(message);
+    
+// }
+
+// showMessage();
+
+// Local Scope
 
 function showMessage() {
+
+    let message = "The Death March"
     console.log(message);
     
 }
 
 showMessage();
+console.log(message);
