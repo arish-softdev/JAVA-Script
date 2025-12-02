4f3773dd0760f27e6ec0de4cd16259eb839baa56
arish-softdev <arish.softdev@gmail.com>
2025-12-02 23:20:37 +0530

    ForEach loop in JavaScript

--- control_flow/forEachLoop.js
+++ control_flow/forEachLoop.js
@@ -1,8 +1,28 @@
 // ForEach loop in JavaScript
 
-const fruits = ["apple", "banana", "orange"];
+// const fruits = ["apple", "banana", "orange"];
 
-fruits.forEach((fruit) => {
-    console.log(fruit);
+// fruits.forEach((fruit) => {
+//     console.log(fruit);
     
-});
+// });
+
+// Ex2
+
+// let numbers = [1, 2, 3, 4];
+
+// numbers.forEach(num => {
+//     console.log(num);
+    
+// })
+
+// Ex3
+
+let arr = [10, 20, 30];
+let sum = 0;
+
+arr.forEach(num => {
+    sum += num;
+})
+
+console.log(sum);
