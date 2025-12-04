06c3c334f31aacad270a21389f4a0112126330ec
arish-softdev <arish.softdev@gmail.com>
2025-12-04 19:19:59 +0530

    Functions in JavaScript

--- functions/midfun.js
+++ functions/midfun.js
@@ -1,13 +1,19 @@
 // Functions in JavaScript
 
-function add(a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//     return a + b;
+// }
 
-console.log(add(5, 10));
+// console.log(add(5, 10));
 
 // Ex1
 
-const greet = () => "Now it's my turn";
+// const greet = () => "Now it's my turn";
 
-console.log(greet());
+// console.log(greet());
+
+// Ex2
+
+const add = (x, y) => x + y;
+
+console.log(add(3, 4));
