b753601448616f50ec43f5a1cbdd5eb64221d66b
arish-softdev <arish.softdev@gmail.com>
2025-12-04 19:21:15 +0530

    Functions in JavaScript

--- functions/midfun.js
+++ functions/midfun.js
@@ -14,6 +14,12 @@
 
 // Ex2
 
-const add = (x, y) => x + y;
+// const add = (x, y) => x + y;
 
-console.log(add(3, 4));
+// console.log(add(3, 4));
+
+// Ex3
+
+let checkEvenOdd = (n) => (n % 2 === 0 ? "Even" : "Odd");
+
+console.log(checkEvenOdd(7));
