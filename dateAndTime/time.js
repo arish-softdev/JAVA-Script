76d099a00fb9b9998871b0f02d71879a096243d8
arish-softdev <arish.softdev@gmail.com>
2025-12-05 17:49:50 +0530

    Dates and Times in JavaScript

--- dateAndTime/time.js
+++ dateAndTime/time.js
@@ -1,21 +1,19 @@
 // 
 
 function updateCurrentDateTime() {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.gettoLocalString("default", { month: "long "});
-    const day = currentDate.getDate();
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
-    
-    
-    const formattedDate = "Formatted Date: " + month + " / " + day + " / " + year;
-    const formattedTime = "Formatted Time: " + hours + " : " + minutes + " : " + seconds;
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = currentDate.toLocaleString("default", { month: "long" });
+      const day = currentDate.getDate();
+      const hours = currentDate.getHours();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds();
 
+      const formattedDate = "Formatted Date: " + month + " " + day + ", " + year;
+      const formattedTime = "Formatted Time: " + hours + ":" + minutes + ":" + seconds;
 
-    document.getElementById("formattedDate").textContent = formattedDate;
-    document.getElementById("formattedTime").textContent = formattedTime;
-}
+      document.getElementById("formattedDate").textContent = formattedDate;
+      document.getElementById("formattedTime").textContent = formattedTime;
+    }
 
-updateCurrentDateTime();
+    updateCurrentDateTime();
