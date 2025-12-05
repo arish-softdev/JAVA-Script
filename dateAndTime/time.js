e7a9bdec7a47d795c839a4594273e2deeb5227aa
arish-softdev <arish.softdev@gmail.com>
2025-12-05 17:47:10 +0530

    Dates and Times in JavaScript

--- dateAndTime/time.js
+++ dateAndTime/time.js
@@ -1,1 +1,21 @@
 // 
+
+function updateCurrentDateTime() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.gettoLocalString("default", { month: "long "});
+    const day = currentDate.getDate();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    
+    
+    const formattedDate = "Formatted Date: " + month + " / " + day + " / " + year;
+    const formattedTime = "Formatted Time: " + hours + " : " + minutes + " : " + seconds;
+
+
+    document.getElementById("formattedDate").textContent = formattedDate;
+    document.getElementById("formattedTime").textContent = formattedTime;
+}
+
+updateCurrentDateTime();
