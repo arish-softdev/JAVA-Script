e0f02c85ac6473de1065c83867021c3c2b4aea0e
arish-softdev <arish.softdev@gmail.com>
2025-12-05 23:20:28 +0530

    Real Clock in Js

---
+++ dateAndTime/rc.js
@@ -0,0 +1,23 @@
+// JavaScript
+
+function updateCurrentDateTime() {
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  var formattedTime =
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+
+    document.getElementById("clock").textContent = "The current is: " + formattedTime;
+}
+
+
+updateCurrentTime();
+
+
+setInterval(updateCurrentTime, 1000);
