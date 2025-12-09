daf824b3baa165594e925fa983ac9c62c78cb780
arish-softdev <arish.softdev@gmail.com>
2025-12-09 23:54:27 +0530

    Ternary Operator in JavaScript

--- conditional_statement/ternary_operator.js
+++ conditional_statement/ternary_operator.js
@@ -55,8 +55,10 @@
 
 // Ex6
 
-let num = -7;
+// let num = -7;
 
-let value = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
+// let value = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
 
-console.log(value);
+// console.log(value);
+
+// Ex7
