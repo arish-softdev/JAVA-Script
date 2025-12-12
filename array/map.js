24aceabc7993fa27396f34b8864db0b567c0b6ef
arish-softdev <arish.softdev@gmail.com>
2025-12-12 22:49:27 +0530

    Map array methods in javascript

--- array/map.js
+++ array/map.js
@@ -8,7 +8,14 @@
 
 // Ex2
 
-let arr = [1, 2, 3, 4];
-let result = arr.map(num => num * 2);
+// let arr = [1, 2, 3, 4];
+// let result = arr.map(num => num * 2);
+
+// console.log(result);
+
+// Ex3
+
+let nums = [10, 20, 30];
+let result = nums.map(n => n + 5);
 
 console.log(result);
