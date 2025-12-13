86ff088e6939a4eafb002cad2ddc4bfce8a95d09
arish-softdev <arish.softdev@gmail.com>
2025-12-13 23:45:20 +0530

    Filter Array Method in JavaScript

--- array/filter.js
+++ array/filter.js
@@ -6,3 +6,11 @@ let numbers = [1, 2, 3, 4, 5, 6];
 let even = numbers.filter(num => num % 2 === 0);
 
 console.log(even);
+
+// Ex2
+
+let names = ["Lemon", "Yanami", "Sayori", "Asuna"];
+
+let longNames = names.filter(name => name.length > 5);
+
+console.log(longNames);
