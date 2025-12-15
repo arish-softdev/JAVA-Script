68c39f18bd9303d346aa967dd165c7fd32db3b58
arish-softdev <arish.softdev@gmail.com>
2025-12-15 23:52:02 +0530

    Click Event in Javascript

--- events/elClickEvent.js
+++ events/elClickEvent.js
@@ -4,3 +4,10 @@ button.addEventListener("click", () => {
     alert("Button clicked!");
 });
 
+// 
+
+let button2 = document.querySelector('#btn2');
+
+button2.addEventListener("mouseover", () => {
+    alert("Mouse over!");
+});
