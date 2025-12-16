2de38d35c7d7612795604c0dfed8b1238db73bff
arish-softdev <arish.softdev@gmail.com>
2025-12-16 23:59:32 +0530

    Async Promise in Javascript

--- async/promise.js
+++ async/promise.js
@@ -24,13 +24,14 @@
 
 // 
 
-let pro = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("Done"), 2000);
-});
+// let pro = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("Done"), 2000);
+// });
 
-console.log("Start");
-pro.then(res => console.log(res));
-console.log("End");
+// console.log("Start");
+// pro.then(res => console.log(res));
+// console.log("End");
 
-
-
+pro.reject("Error")
+  .then(() => console.log("Success"))
+  .catch(err => console.log(err));
