e179046bb14d2c62ff12a899652c162e7ca4f984
arish-softdev <arish.softdev@gmail.com>
2025-12-18 23:56:21 +0530

    Filter Array Method in JavaScript

--- array/filter.js
+++ array/filter.js
@@ -1,36 +1,38 @@
 // Filter Array Method in JavaScript
 
-// Ex1 Even Nums
+// // Ex1 Even Nums
 
-let numbers = [1, 2, 3, 4, 5, 6];
-let even = numbers.filter(num => num % 2 === 0);
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let even = numbers.filter(num => num % 2 === 0);
 
-console.log(even);
+// console.log(even);
 
 // Ex2 Length
 
-let names = ["Lemon", "Yanami", "Sayori", "Asuna"];
+// let names = ["Lemon", "Yanami", "Sayori", "Asuna"];
 
-let longNames = names.filter(name => name.length > 5);
+// let longNames = names.filter(name => name.length > 5);
 
-console.log(longNames);
+// console.log(longNames);
 
 // Ex3 Positive nums
 
-let nums = [-3, 5, -7, 1, 0];
+// let nums = [-3, 5, -7, 1, 0];
 
-let positive = nums.filter(n => n > 0);
+// let positive = nums.filter(n => n > 0);
 
-console.log(positive);
+// console.log(positive);
 
 // Negative
 
-let num = [-3, 5, -7, 1, 0];
+// let num = [-3, 5, -7, 1, 0];
 
-let negative = num.filter(n => n < 0);
+// let negative = num.filter(n => n < 0);
 
-console.log(negative);
+// console.log(negative);
 
 // Ex4 Greater Nums
 
-let
+let nums = [5, 12, 8, 20, 3];
+
+let 
