8b5df3adb55e77eb70cb659ee0454041bcc29232
arish-softdev <arish.softdev@gmail.com>
2025-12-18 23:47:28 +0530

    Async Promise in Javascript

--- async/promise.js
+++ async/promise.js
@@ -103,3 +103,12 @@ getData()
   .catch(err => console.log(err));
 
 // Ex9
+
+let p1 = Promise.resolve(10);
+let p2 = Promise.resolve(20);
+let p3 = Promise.resolve(30);
+
+
+Promise.all([p1, p2, p3])
+  .then(values => console.log(values)
+  );
