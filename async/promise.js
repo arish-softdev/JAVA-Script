8770f13346def27d2d6870be2415c62d16f0c306
arish-softdev <arish.softdev@gmail.com>
2025-12-26 22:14:55 +0530

    Async Promise in Javascript

--- async/promise.js
+++ async/promise.js
@@ -203,5 +203,5 @@ let p1 = Promise.resolve("HTML");
 let p2 = Promise.resolve("CSS");
 let p3 = Promise.resolve("JavaScript");
 
-promise.all([p1, p2, p3])
+Promise.all([p1, p2, p3])
 .then(values => console.log(values));
