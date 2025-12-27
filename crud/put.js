74d55a5fbb270989121b3eb0254d487371a8c373
arish-softdev <arish.softdev@gmail.com>
2025-12-27 19:12:50 +0530

    Crud in JavaScript

---
+++ crud/put.js
@@ -0,0 +1,16 @@
+function updateProduct(e) {
+  e.preventDefault();
+
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      title: document.getElementById("product").value
+    })
+  })
+  .then(res => res.json())
+  .then(data => alert("Updated Successfully"))
+  .catch(err => console.log(err));
+}
