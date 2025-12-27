33cd4d6f0dde88a92312b20d7cc5b0733f8dff9b
arish-softdev <arish.softdev@gmail.com>
2025-12-27 18:49:03 +0530

    Crud in JavaScript

---
+++ crud/crud.js
@@ -0,0 +1,15 @@
+let button = document.querySelector("#fetch-button");
+button.addEventListener("click", generator);
+
+function generator() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.querySelector("#randomimage").src = data.message;
+    })
+    .catch((error) => {
+      console.log("error fetching image:", error);
+    });
+}
